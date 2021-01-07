7daae0614af7b8cb5f793e6efba71020683cc95e
MonseGlez <monserrat.gonzalez95@gmail.com>
2021-01-06 18:45:50 -0600

    Proyecto web

---
+++ proyecto/perfil/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+from .models import Perfil
+
+class SignUpForm(UserCreationForm):
+    Nombre = forms.CharField(max_length=140, required=True)
+    Apellido = forms.CharField(max_length=140, required=False)
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'Nombre',
+            'Apellido',
+            'password1',
+            'password2',
+        )
+
